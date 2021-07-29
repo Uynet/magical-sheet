34d5885d6d75f769028cccf5bbee0125e83a7299
uynet <highsate@gmail.com>
2021-07-29 16:15:54 +0900

    get-datas

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import axiosBase from "axios";
+
+const api =
+  "https://script.googleusercontent.com/macros/echo?user_content_key=0fUxMW4mpkAyqS0ITItiV5PWMDRXpHW-zrtnWbHRcgX3-5zU2LhWWiq8iFXq2dyJMdV65EXDc6_tnVMg0nVxkPpLG5C5yhnIm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnEW-DLxYGLjYQy9XCtWxk3oO7qP1OkYMqXq_6MWsaFtqQ78f0v5BuIuOBiYQdpam5bZR2sTbrv4X_oUk-9xy9RDeuEw2opXKBw&lib=MpX9rb-90rMYO9S6HI_TeypeSVVadBdJf";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
